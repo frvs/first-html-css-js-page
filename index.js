05dadcf63fab4414615889c13731f3b0f74cc369
lucas.frois <lucas.frois@fastchannel.com>
2021-03-08 22:54:21 -0300

    >

---
+++ index.js
@@ -0,0 +1,35 @@
+function inscreverUsuario() {
+    let email = document.getElementById('email').value
+
+    alert('Obrigado por se inscrever, ' + email)
+}
+
+// let variavel = 'Variavel'
+// variavel = 1
+
+// function minhaFuncao(parametro1, parametro2, parametro3) {
+//     return parametro1 + parametro2 + parametro3
+// }
+
+// let meuRetorno = minhaFuncao(1, 2, 3)
+
+// alert("Oi")
+// console.log("Oi")
+
+
+// // tipos simples
+
+// let numero = 1.21
+// let texto = "hello"
+// let ehSegunda = true
+
+// // tipos complexos
+// let numerosDaMegaSena = [10, 32, 45, 78, 23]
+// primeroNumero = numerosDaMegaSena[0] // 10
+
+// let pessoa = {
+//     nome: 'Lucas', 
+//     idade: 21, 
+//     lambder: true, 
+//     cigarrosPorDia: [10, 11, 12, 8]
+// }
